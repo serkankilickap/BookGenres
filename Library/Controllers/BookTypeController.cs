2c5c94b40f1f35cf788b2fb18bd98aa8916b1554
Serkan Kilickap <serkankilickap@gmail.com>
2024-01-16 16:55:32 +0300

    Add project files.

---
+++ Library/Controllers/BookTypeController.cs
@@ -0,0 +1,20 @@
+﻿using Library.Models;
+using Library.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class BookTypeController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+        public BookTypeController(AppDbContext context)
+        {
+            _appDbContext = context;
+        }
+        public IActionResult Index()
+        {
+            List<BookType> objBookTypeList = _appDbContext.BookTypes.ToList();
+            return View(objBookTypeList);
+        }
+    }
+}
---
+++ Library/Utility/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Utility
+{
+    public class AppDbContext : DbContext
+
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<BookType> BookTypes { get; set; }
+    }
+}
